5d7e2f569ad17d58455b42ff783c06a9aaff3afd
alantheandroid <alessio.ilardi@gmail.com>
2022-06-18 16:38:12 +0200

    feat: add requested spread syntax

--- exercise-1.js
+++ exercise-1.js
@@ -1,5 +1,5 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+function sum(...numbers) {
+  return numbers.reduce((result, value) => result + value);
 }
 
 console.log(sum(1, 2, 3, 4, 5));
